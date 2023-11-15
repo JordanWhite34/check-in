56bb6251dbf5d8ba771c4630857cb482b28911cd
Jordan White <jwhite34@uw.edu>
2023-11-14 18:38:42 -0800

    changed usage of pillow to cv2

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,51 +1,53 @@
 # preprocessing.py
 import os
 import cv2
+import numpy as np
 import torch
-from torchvision import transforms
 from config import PREPROCESSING_CONFIG, DATA_PATHS
 
 
-# Function to load an image using OpenCV and convert it to RGB
 def load_image(image_path):
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert from BGR to RGB
     return image
 
 
-# Function to apply transformations to an image
 def transform_image(image):
-    transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize(PREPROCESSING_CONFIG['image_size']),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=PREPROCESSING_CONFIG['mean'],
-            std=PREPROCESSING_CONFIG['std']
-        )
-    ])
-    return transform(image)
+    # Resize image using OpenCV
+    resized_image = cv2.resize(image, PREPROCESSING_CONFIG['image_size'])
+
+    # Convert to float and normalize
+    resized_image = resized_image.astype(np.float32) / 255.
+    mean = np.array(PREPROCESSING_CONFIG['mean'])
+    std = np.array(PREPROCESSING_CONFIG['std'])
+    normalized_image = (resized_image - mean) / std
+
+    return normalized_image
 
 
-# Function to preprocess all images in a directory
 def preprocess_directory(directory_path, output_directory):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
     for filename in os.listdir(directory_path):
-        if filename.lower().endswith(('.jpg', '.png', '.jpeg')):  # Add other formats as needed
+        if filename.lower().endswith(('.jpg', '.png', '.jpeg')):
             input_path = os.path.join(directory_path, filename)
             output_path = os.path.join(output_directory, filename)
 
             try:
                 image = load_image(input_path)
                 transformed_image = transform_image(image)
-                torch.save(transformed_image, output_path.replace('.jpg', '.pt'))  # Saving as PyTorch tensor
+
+                # Convert normalized image back to 0-255 range for saving
+                transformed_image = np.clip(transformed_image, 0, 1)
+                transformed_image = (transformed_image * 255).astype(np.uint8)
+
+                # Save the image using OpenCV
+                cv2.imwrite(output_path, cv2.cvtColor(transformed_image, cv2.COLOR_RGB2BGR))
             except Exception as e:
                 print(f"Error processing {filename}: {e}")
 
 
-# Main function to run the preprocessing
 if __name__ == "__main__":
     for category in ['clean', 'messy']:
         input_dir = os.path.join(DATA_PATHS['raw'], category)
