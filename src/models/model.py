28cfc4bb7379a85c8a4c17e339fcd8a2b08f7720
Jordan White <jwhite34@uw.edu>
2023-11-21 11:09:08 -0800

    initial model file

---
+++ src/models/model.py
@@ -0,0 +1,21 @@
+# model.py
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.fc1 = nn.Linear(64 * 56 * 56, 512)  # Adjust the input features to match your image size
+        self.fc2 = nn.Linear(512, 2)  # Assuming binary classification (clean vs messy)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 56 * 56)  # Flatten and adjust to match your image size and conv layers
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
