ac828b93c6c08d5a544c9e0b208961bc6a485821
Jordan White <jwhite34@uw.edu>
2023-11-21 13:07:04 -0800

    added dropout

--- src/models/model.py
+++ src/models/model.py
@@ -6,16 +6,20 @@ import torch.nn.functional as F
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.fc1 = nn.Linear(64 * 56 * 56, 512)  # Adjust the input features to match your image size
-        self.fc2 = nn.Linear(512, 2)  # Assuming binary classification (clean vs messy)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 56 * 56, 512)
+        self.dropout1 = nn.Dropout(p=0.25)
+        self.fc2 = nn.Linear(512, 2)
+        self.dropout2 = nn.Dropout(p=0.25)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 64 * 56 * 56)  # Flatten and adjust to match your image size and conv layers
+        x = x.view(-1, 64 * 56 * 56)
         x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
         x = self.fc2(x)
+        x = self.dropout2(x)
         return x
