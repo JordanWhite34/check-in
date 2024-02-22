86dd466ca3772092d5bbc23881fca186e45d0263
Jordan White <jwhite34@uw.edu>
2024-02-21 19:05:46 -0800

    added model_path to MODEL_PARAMS

--- src/config.py
+++ src/config.py
@@ -19,7 +19,8 @@ MODEL_PARAMS = {
     'learning_rate': 1e-4,  # A good starting point for many models
     'batch_size': 32,  # Adjust based on memory availability
     'num_epochs': 25,  # Initial epochs count
-    'weight_decay': 1e-4
+    'weight_decay': 1e-4,
+    'model_path': 'checkpoints/checkpoint_epoch_20.pth.tar'
 }
 
 # Training Settings
